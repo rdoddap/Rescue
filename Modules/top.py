cc8dc0fb8f99f841f2c5d042a2303d0327dd7017
rdoddap <50403178+rdoddap@users.noreply.github.com>
2023-11-27 11:59:49 +0530

    top modified

--- Modules/top.py
+++ Modules/top.py
@@ -1,6 +1,13 @@
 import run_command
 
+
 def linux_top():
-    return run_command("top -b -n 1")
+  return run_command.run_command("top -b -n 1")
+
+
 def linux_ipconfig():
-    return run_command("ipconfig -a")
+  return run_command.run_command("ipconfig -a")
+
+
+linux_top()
+linux_ipconfig()
